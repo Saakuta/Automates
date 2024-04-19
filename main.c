41485b59d422231b28400b9af0a0ec31fe8af16d
Sakutaa <jonathan.rajoeliarivony@gmail.com>
2024-04-19 16:07:19 +0200

    main.c et main.h

---
+++ main.c
@@ -0,0 +1,5 @@
+//
+// Created by Jonathan R. on 19/04/2024.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by Jonathan R. on 19/04/2024.
+//
+
+#ifndef AUTOMATES_MAIN_H
+#define AUTOMATES_MAIN_H
+
+#endif //AUTOMATES_MAIN_H
