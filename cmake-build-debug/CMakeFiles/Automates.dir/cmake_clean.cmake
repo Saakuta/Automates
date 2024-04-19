dd2ab58441b73d5ae74081eacf88fc3797052ec2
LoKS <thomas.brandy@efrei.net>
2024-04-19 19:35:29 +0200

    Ajout des données des automates 10-30 et création fichier 20-44

---
+++ cmake-build-debug/CMakeFiles/Automates.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Automates.exe"
+  "Automates.exe.manifest"
+  "Automates.pdb"
+  "CMakeFiles/Automates.dir/main.c.obj"
+  "CMakeFiles/Automates.dir/main.c.obj.d"
+  "libAutomates.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Automates.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Automates.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Automates.
+# This may be replaced when dependencies are built.
